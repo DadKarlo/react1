76fd6cf41e70d2975e9b140397f66ff11ff56495
PapaCarlo <dimon9608@bk.ru>
2026-03-05 22:56:31 +0300

    1.0.1

--- API/server.js
+++ API/server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true })) //!!! обработка фор�
 app.use(express.json()) //!!! обязательно для body запроса !!!
 const API = '/apidimon08041996reostat12'
 const APIALL = 'http://localhost:5000/apidimon08041996reostat12'
-const prodaction = 'http://localhost:3000/'
+const prodaction = 'http://155.212.239.181/api/'
 const port = process.env.PORT || 5000
 
 // Сообщение о том, что сервер запущен и прослушивает указанный порт
--- SITE/src/API_URL.js
+++ SITE/src/API_URL.js
@@ -1,2 +1,2 @@
-// export const API_site = 'http://178.72.152.182:5000'
-export const API_site = 'http://localhost:5000'
+export const API_site = 'http://155.212.239.181'
+// export const API_site = 'http://localhost:5000'
